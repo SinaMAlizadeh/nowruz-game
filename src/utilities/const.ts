a8e0154715af9605d46dae54f00ba10ef373b592
sina alizadeh <32089050+SinaMAlizadeh@users.noreply.github.com>
2024-03-12 15:11:51 +0330

    FIX: change winnner score

--- src/utilities/const.ts
+++ src/utilities/const.ts
@@ -2,7 +2,7 @@ import { Live } from "../models/live";
 export const RAISE_LEVEL_TIMER = 20000;
 export const REDUCE_TIMER = 0.5;
 export const GITHUB_URL = "https://github.com/SinaMAlizadeh/nowruz-game";
-export const WINNER_POINTER = 20;
+export const WINNER_POINTER = 1403;
 
 export const lives: Array<Live> = [
   {
